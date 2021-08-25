861b605cd9390014c30aaed36324b7b568520227
Ryan Erricson <ryanerricson@gmail.com>
2021-08-25 18:06:02 -0400

    Connected to MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const connectDB = require('./config/db')
+
 
 // Route files
 const bootcamps = require('./routes/bootcamps')
+const { request } = require('express')
 //Load environment
 dotenv.config({ path: './config/config.env'})
+
+connectDB()
 
 const app = express()
 
@@ -20,3 +25,7 @@ app.use('/api/v1/bootcamps', bootcamps)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+})
