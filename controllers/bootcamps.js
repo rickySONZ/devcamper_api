785f506b87c9ee84f4d4c7f2517d06753e9def10
Ryan Erricson <ryanerricson@gmail.com>
2021-08-26 16:43:10 -0400

    Full CRUD functionality

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,7 +10,7 @@ exports.getBootcamps = async (req, res, next) => {
     try {
         const bootcamps = await Bootcamp.find();
         
-        res.status(200).json({ success: true, data: bootcamps})
+        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps})
     } catch (error) {
         res.status(400).json({ success: false })
     }
@@ -54,23 +54,39 @@ exports.createBootcamp = async (req, res, next) => {
 // @route   PATCH /api/v1/bootcamps
 // @access  Private
 exports.updateBootcamp = async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    })
 
-    if(!bootcamp) {
-        return res.status(400).json({success: false})
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+    
+        if(!bootcamp) {
+            return res.status(400).json({success: false})
+        }
+    
+        res.status(200).json({ success: true, data: bootcamp })
+    } catch (error) {
+        res.status(400).json({success: false})
     }
-
-    res.status(200).json({ success: true, data: bootcamp })
 }
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps
 // @access  Private
-exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}`})
+exports.deleteBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+    
+        if(!bootcamp) {
+            return res.status(400).json({success: false})
+        }
+    
+        res.status(200).json({ success: true, data: {} })
+    } catch (error) {
+        res.status(400).json({success: false})
+    }
+
 }
 
 
