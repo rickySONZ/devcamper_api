27c555f41fe19fab62e08fed9d8353ee5fc2ef38
Ryan Erricson <ryanerricson@gmail.com>
2021-08-30 13:40:06 -0400

    Added method to register new users

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,5 +9,16 @@ const path = require('path')
 //@access   Public
 
 exports.register = asyncHandler(async (req, res, next) => {
-    res.status(200).json({ success: true })
+    const { name, email, password, role } = req.body
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
+    })
+
+    res.status(200).json({
+        success: true
+    })
 })
