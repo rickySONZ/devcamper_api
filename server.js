652b9a76c8dce4488df64e276a7ec17edbc23355
Ryan Erricson <ryanerricson@gmail.com>
2021-08-30 13:08:32 -0400

    Created user model and basic routing structure

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const path = require('path')
 // Route files
 const bootcamps = require('./routes/bootcamps')
 const courses = require('./routes/courses')
+const auth = require('./routes/auth')
 
 
 //Load environment
@@ -37,6 +38,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
+app.use('/api/v1/auth', auth)
+
 
 app.use(errorHandler)
 
