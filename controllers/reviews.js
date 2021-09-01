b6ab20d2bbd482d63f45f0fa5f15877bd135db74
Ryan Erricson <ryanerricson@gmail.com>
2021-09-01 15:21:47 -0400

    Can now add single review and seeder has been updated

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,6 +3,10 @@ const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
 const Bootcamp = require('../models/Bootcamp')
 
+// @desc    Get all Reviews/ revies for single bootcamp
+//@route    GET /api/v1/reviews   /api/v1/bootcamps/:bootcampId/reviews
+//@access   PUBLIC
+
 exports.getReviews = asyncHandler(async (req, res, next) => {
 
     if (req.params.bootcampId) {
@@ -17,5 +21,23 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
         //access to advanced results is because route is wrapped in middleware
         res.status(200).json(res.advancedResults)
     }
+
+})
+
+exports.getReview = asyncHandler(async (req, res, next) => {
+    const review = await Review.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: 'name description'
+    })
+
+    if(!review) {
+        return next(new ErrorResponse(`No review found with the id of ${req.params.id}`, 404))
+    }
+
+    res.status(200).json({
+        success: true,
+        data: review
+    })
+
 
 })
