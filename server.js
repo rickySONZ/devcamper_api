3f7a6aec79bc66e65ed87760b591b0308080cb3b
Ryan Erricson <ryanerricson@gmail.com>
2021-09-01 14:35:40 -0400

    Added admin functionality on users

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const path = require('path')
 const bootcamps = require('./routes/bootcamps')
 const courses = require('./routes/courses')
 const auth = require('./routes/auth')
+const users = require('./routes/users')
 
 
 //Load environment
@@ -43,6 +44,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
 app.use('/api/v1/auth', auth)
+app.use('/api/v1/users', users)
+
 
 
 app.use(errorHandler)
